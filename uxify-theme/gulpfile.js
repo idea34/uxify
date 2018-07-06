1a1ab771a72e3d8e8427b1e34b47de8845d8ca1a
Mike Wilson <idea34@gmail.com>
2018-07-06 16:10:39 -0400

    Setup theme specific builds and web preview
    - add local webserver
    - build basic dev task
    - create theme build directory structure

--- uxify-theme/gulpfile.js
+++ uxify-theme/gulpfile.js
@@ -1,13 +1,24 @@
-var gulp  = require('gulp'),
+// configs
+var
+  themesdir = './themes/',
+  theme = 'default',
+  themepath = themesdir + theme + '/',
+  themebuild = themepath + 'build/'
+
+// modules
+var
+  gulp  = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   cleanCss = require('gulp-clean-css'),
   rename = require('gulp-rename'),
   postcss      = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  server = require('gulp-server-livereload');
+
 
 gulp.task('build-theme', function() {
-  return gulp.src(['scss/*.scss'])
+  return gulp.src([ themepath + '*.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([ autoprefixer({ browsers: [
@@ -21,15 +32,29 @@ gulp.task('build-theme', function() {
       'Android >= 4',
       'Opera >= 12']})]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest(themebuild))
     .pipe(cleanCss())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest(themebuild))
 });
 
-gulp.task('watch', ['build-theme'], function() {
-  gulp.watch(['scss/*.scss'], ['build-theme']);
+gulp.task('dev', ['build-theme', 'webserver'],  function() {
+  gulp.watch([themepath + '*.scss'], ['build-theme']);
 });
 
-gulp.task('default', ['build-theme'], function() {
+gulp.task('default', ['dev'], function() {
+});
+
+//
+// #### dev server
+// options: https://www.npmjs.com/package/gulp-server-livereload
+gulp.task('webserver', function() {
+  gulp.src( themepath  )
+    .pipe(server({
+      livereload: true,
+      open: true,
+      port: 8180,
+      defaultFile: 'index.html',
+      fallback: 'index.html'
+    }));
 });
